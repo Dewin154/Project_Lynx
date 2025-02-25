9c39c04e7dad6ecf4df8fb15f932703f4f11ccff
Dewin154 <peterokruhlica@gmail.com>
2025-02-25 16:49:17 +0100

    Logic for SwordHitbox added

--- player/sword_hitbox.gd
+++ player/sword_hitbox.gd
@@ -2,7 +2,12 @@ extends Area2D
 
 @onready var player = get_parent()
 
-func _on_body_entered(body: Node2D) -> void:
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("attack"):
+		activate_hitbox()
+
+func activate_hitbox():
+	for body in get_overlapping_bodies():
 		if body is Snail:
 			body.take_damage(10)
-	
