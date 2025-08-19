e0fb8636052f441eb333040d21fa1985d802d395
Peter Okruhlica <peterokruhlica@gmail.com>
2025-08-19 13:17:24 +0200

    Implemented Bee sprite flip

--- enemies/bee/bee.gd
+++ enemies/bee/bee.gd
@@ -1,45 +1,27 @@
 class_name Bee
-extends AbstractEnemies
+extends CharacterBody2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var reverse_movement_timer: Timer = $ReverseMovementTimer
-var fly_forward = false
-var fly_velocity = 25 
+@onready var animated_sprite_2d: AnimatedSprite2D = $Path2D/PathFollow2D/Area2D/AnimatedSprite2D
+@onready var path_follow := $Path2D/PathFollow2D
 
 enum State {Fly, Attack, Hit}
 
+var speed = 100.0
 var health = 40
-var temp = 0
-
+var current_state
+var last_x = 0
+var current_x
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity.x = -fly_velocity
+	current_state = State.Fly
+	pass
 
+# TODO Optimize this since it checks every frame!
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	move(delta)
-	
-func _on_reverse_movement_timer_timeout() -> void:
-	fly_forward = !fly_forward
-	if fly_forward:
-		velocity.x = fly_velocity
-	else:
-		velocity.x = -fly_velocity
-	animated_sprite_2d.flip_h = true if velocity.x > 0 else false
-
-# Must be overriden, otherwise gravity is applied to bee
-func falling(delta: float) -> void:
-	pass
-
-# Controls the flying of the bee and as such the movement of the bee
-func move(delta: float):
-	temp = sin(position.x) + 150
-	position.y = temp
-
-func take_damage(damage):
-	pass
-
-func play_animations() -> void:
-	pass	
+	current_x = path_follow.global_position.x
+	path_follow.progress += speed * delta
+	animated_sprite_2d.flip_h = false if current_x < last_x else true
+	last_x = current_x
 	
